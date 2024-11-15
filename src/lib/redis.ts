0ae5549ff6c2304c7ef7474ca72465fcf4a725aa
thalysson-nascimento <thalyssonwg.nascimento@gmail.com>
2024-11-15 17:34:32 -0300

    feat: incluido variaveis de ambiente para o redis

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,12 +1,15 @@
 // lib/redis.ts
 import { createClient } from "redis";
 
+const redisHost = process.env.REDIS_HOST;
+const redisPort = process.env.REDIS_PORT;
+
 // Cliente padrão para leitura/escrita
-const client = createClient({ url: "redis://localhost:6379" });
+const client = createClient({ url: `${redisHost}:${redisPort}` });
 client.on("error", (err) => console.log("Redis Client Error", err));
 
 // Cliente exclusivo para assinaturas
-const subscriberClient = createClient({ url: "redis://localhost:6379" });
+const subscriberClient = createClient({ url: `${redisHost}:${redisPort}` });
 subscriberClient.on("error", (err) =>
   console.log("Redis Subscriber Client Error", err)
 );
