c79ee265b167a87c9313fbc4a33d2d0ffe7be722
thalysson-nascimento <thalyssonwg.nascimento@gmail.com>
2025-02-01 19:30:04 -0300

    feat: removido bloco de tratamento try

--- src/modules/account/create-account-with-google/useCase/create-account-with-googleUseCase.ts
+++ src/modules/account/create-account-with-google/useCase/create-account-with-googleUseCase.ts
@@ -18,152 +18,130 @@ export class CreateAccountWithGoogleUseCase {
   }
 
   async execute(tokenGoogleOAuth: string) {
-    try {
-      const ticket = await client.verifyIdToken({
-        idToken: tokenGoogleOAuth,
-        audience: process.env.GOOGLE_CLIENT_ID,
-      });
+    const ticket = await client.verifyIdToken({
+      idToken: tokenGoogleOAuth,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
 
-      const payload = ticket.getPayload();
+    const payload = ticket.getPayload();
 
-      if (!payload) {
-        throw createHttpError(401, "Invalid token.");
-      }
+    if (!payload) {
+      throw createHttpError(401, "Invalid token.");
+    }
 
-      const { sub, name, email, picture, exp } = payload;
+    const { sub, name, email, picture, exp } = payload;
 
-      // Verifica se o token já expirou
-      const now = Math.floor(Date.now() / 1000);
-      if (exp && exp < now) {
-        throw createHttpError(401, "Token expired. Please authenticate again.");
-      }
+    // Verifica se o token já expirou
+    const now = Math.floor(Date.now() / 1000);
+    if (exp && exp < now) {
+      throw createHttpError(401, "Token expirado.");
+    }
 
-      if (!name) {
-        throw createHttpError(400, "Name is required for user creation.");
-      }
+    if (!name) {
+      throw createHttpError(400, "Nome é obrigatório.");
+    }
 
-      if (!email) {
-        throw createHttpError(400, "Email is required for user creation.");
-      }
+    if (!email) {
+      throw createHttpError(400, "Email é obrigatório.");
+    }
 
-      if (!picture) {
-        throw createHttpError(400, "Picture is required for user creation.");
-      }
+    if (!picture) {
+      throw createHttpError(400, "Foto é obrigatória.");
+    }
 
-      const user = await prisma.user.findUnique({ where: { email } });
+    const emailUserAdmin = await prisma.user.findFirst({
+      where: {
+        email: {
+          equals: email,
+          mode: "insensitive",
+        },
+      },
+    });
 
-      if (user) {
-        throw createHttpError(409, "User already exists.");
-      }
+    if (emailUserAdmin) {
+      throw createHttpError(409, "Usuário existente");
+    }
 
-      const emailUserAdmin = await prisma.user.findFirst({
-        where: {
-          email: {
-            equals: email,
-            mode: "insensitive",
+    const createUserUseCase = new CreateUserUseCase();
+    await createUserUseCase.execute({
+      name,
+      email,
+      userHashPublic: generateUniqueHash(),
+    });
+
+    const userClient = await prisma.user.findFirst({
+      where: { email },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        userHashPublic: true,
+        isUploadAvatar: true,
+        verificationTokenEmail: true,
+        validatorLocation: true,
+        ActivePlanGolFreeTrial: true,
+        SubscriptionPlanGoldFreeTrial: {
+          select: {
+            firstPublicationPostMessage: true,
+            viewCardFreeTrial: true,
           },
         },
-      });
-
-      if (emailUserAdmin) {
-        throw createHttpError(
-          409,
-          "o email informado está em uso, tente outro"
-        );
-      }
-
-      const createUserUseCase = new CreateUserUseCase();
-      await createUserUseCase.execute({
-        name,
-        email,
-        userHashPublic: generateUniqueHash(),
-      });
-
-      const userClient = await prisma.user.findFirst({
-        where: { email },
-        select: {
-          id: true,
-          name: true,
-          email: true,
-          userHashPublic: true,
-          isUploadAvatar: true,
-          verificationTokenEmail: true,
-          validatorLocation: true,
-          ActivePlanGolFreeTrial: true,
-          SubscriptionPlanGoldFreeTrial: {
-            select: {
-              firstPublicationPostMessage: true,
-              viewCardFreeTrial: true,
-            },
-          },
-          avatar: {
-            select: {
-              image: true,
-              createdAt: true,
-              user: {
-                select: {
-                  name: true,
-                  email: true,
-                },
+        avatar: {
+          select: {
+            image: true,
+            createdAt: true,
+            user: {
+              select: {
+                name: true,
+                email: true,
               },
             },
           },
         },
-      });
+      },
+    });
 
-      if (!userClient?.id) {
-        throw createHttpError(404, "User not found");
-      }
-
-      await this.repository.createOAuthUser(
-        sub,
-        "google",
-        userClient.id,
-        picture
-      );
-
-      const token = sign(
-        { email },
-        "dff2f370b3331305c51daafbdf7d2b6e-user-admin",
-        {
-          subject: userClient.id,
-          expiresIn: "1d",
-        }
-      );
-
-      const planGoldFreeTrial = new PlanGoldFreeTrial();
-      const goldFreeTrialData = await planGoldFreeTrial.activePlan(
-        userClient.id
-      );
-
-      return {
-        token,
-        expiresIn: "1d",
-        userVerificationData: {
-          userHashPublic: userClient.userHashPublic,
-          isUploadAvatar: userClient.isUploadAvatar,
-          verificationTokenEmail: userClient.verificationTokenEmail,
-          validatorLocation: userClient.validatorLocation,
-        },
-        avatar: {
-          image: userClient?.avatar?.image,
-          createdAt: userClient?.avatar?.createdAt,
-          user: {
-            name: userClient.name,
-            email: userClient.email,
-          },
-        },
-        goldFreeTrialData,
-      };
-    } catch (error: any) {
-      if (
-        error.message.includes("Token used too late") ||
-        error.message.includes("Token expired")
-      ) {
-        throw createHttpError(401, "Token expired. Please authenticate again.");
-      }
-
-      throw createHttpError(500, "Internal server error.");
+    if (!userClient?.id) {
+      throw createHttpError(404, "User not found");
     }
+
+    await this.repository.createOAuthUser(
+      sub,
+      "google",
+      userClient.id,
+      picture
+    );
+
+    const token = sign(
+      { email },
+      "dff2f370b3331305c51daafbdf7d2b6e-user-admin",
+      {
+        subject: userClient.id,
+        expiresIn: "1d",
+      }
+    );
+
+    const planGoldFreeTrial = new PlanGoldFreeTrial();
+    const goldFreeTrialData = await planGoldFreeTrial.activePlan(userClient.id);
+
+    return {
+      token,
+      expiresIn: "1d",
+      userVerificationData: {
+        userHashPublic: userClient.userHashPublic,
+        isUploadAvatar: userClient.isUploadAvatar,
+        verificationTokenEmail: userClient.verificationTokenEmail,
+        validatorLocation: userClient.validatorLocation,
+      },
+      avatar: {
+        image: userClient?.avatar?.image,
+        createdAt: userClient?.avatar?.createdAt,
+        user: {
+          name: userClient.name,
+          email: userClient.email,
+        },
+      },
+      goldFreeTrialData,
+    };
   }
 }
